4f7305adba74c7ec817ac80fc828ea7059ecddec
npmasimula20@gmail.com <npmasimula20@gmail.com>
2022-11-04 14:27:58 +0200

    variable name changed

--- casinogame.py
+++ casinogame.py
@@ -1,21 +1,21 @@
 import random
         
-wager_input = int(input("Enter a wager amount: R"))
+balance = int(input("Enter a wager amount: R"))
 comp_num = random.randint(1, 10)
 
-while wager_input > 0:
+while balance > 0:
     user_input = int(input("Enter a number between 1 and 10: "))
     if user_input == comp_num:
-        wager_input += 1
+        balance += 1
         print("Congratulations, You have won!!!")
         
     elif user_input != comp_num:
-        wager_input -= 1
+        balance -= 1
         print("Oops, You have lost, try again!!!")
         print("The number you have guessed was: ", user_input)
         print("The computer number was: ", comp_num)
-        print("You have: R" + str(wager_input) + " funds remaining")
+        print("You have: R" + str(balance) + " funds remaining")
         
-if wager_input == 0:
+if balance == 0:
     print("Sorry, You have insufficient funds, Bye!")
         
